d6b8871e31487ff7ebf26e1f71fd6402b42acfaa
Prem Mhatre <83350182+Prem-Mhatre@users.noreply.github.com>
2021-10-06 20:09:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,10 +5,28 @@ function preload(){
 function setup(){
     canvas = createCanvas(300, 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    pose_net = ml5.poseNet(video, modelLoaded);
+    pose_net.on('pose', gotPose);
+}
+
+function modelLoaded(){
+    console.log("pose model Loaded");
+}
+
+function gotPose(results){
+    if(results.length > 0){
+        console.log(results);
+        console.log("noseX: " + results[0].pose.nose.x);
+        console.log("noseY: " + results[0].pose.nose.y);
+    }
 }
 
 function draw(){
-
+    image(video, 0, 0, 300, 300);
 }
 
 function takeSnapshot(){
